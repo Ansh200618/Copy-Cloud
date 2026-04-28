e3d43481d78f07f315bdc2ae65d2d1ab0fea40fe
Anshdeep Singh <197048808+Ansh200618@users.noreply.github.com>
2026-04-28 23:21:09 +0530

    Turned on Vercel Speed Insights

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,5 @@
-
+import { injectSpeedInsights } from '@vercel/speed-insights';
+injectSpeedInsights();
   import { createRoot } from "react-dom/client";
   import App from "./app/App.tsx";
   import "./styles/index.css";
